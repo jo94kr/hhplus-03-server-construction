23c35dd2993d9f7ae5bab8869af1249c172ad6cf
jo94kr <wls94kr@gmail.com>
2024-06-30 17:06:43 +0900

    init

---
+++ src/main/java/io/hhplus/server_construction/ServerConstructionApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.server_construction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConstructionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerConstructionApplication.class, args);
+    }
+
+}
