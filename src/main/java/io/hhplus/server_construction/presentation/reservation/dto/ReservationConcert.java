10bb1fd9c318808929f07086bc359d36cd06a00e
jo94kr <wls94kr@gmail.com>
2024-07-12 14:06:22 +0900

    feat: 예약 기능, 테스트

--- src/main/java/io/hhplus/server_construction/presentation/reservation/dto/ReservationConcert.java
+++ src/main/java/io/hhplus/server_construction/presentation/reservation/dto/ReservationConcert.java
@@ -1,6 +1,10 @@
 package io.hhplus.server_construction.presentation.reservation.dto;
 
+import io.hhplus.server_construction.application.reservation.dto.ReservationConcertResult;
+import io.hhplus.server_construction.domain.reservation.ReservationItem;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public record ReservationConcert(
@@ -19,11 +23,25 @@ public record ReservationConcert(
             List<Item> reservationItemList
     ) {
 
+        public static Response from(ReservationConcertResult reservationConcertResult) {
+            return new Response(reservationConcertResult.reservationId(),
+                    reservationConcertResult.totalPrice(),
+                    reservationConcertResult.reservationItemList().stream()
+                            .map(Item::from)
+                            .toList());
+        }
+
         public record Item(
                 Long concertSeatId,
                 String seatNum,
                 BigDecimal price
         ) {
+
+            public static Item from(ReservationItem reservationItem) {
+                return new Item(reservationItem.getConcertSeat().getId(),
+                        reservationItem.getConcertSeat().getSeatNum(),
+                        reservationItem.getPrice());
+            }
         }
     }
 
