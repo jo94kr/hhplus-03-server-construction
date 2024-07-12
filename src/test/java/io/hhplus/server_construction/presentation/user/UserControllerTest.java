6b7268c0fc7cf2d59e506807c03af7e0a3c8f693
jo94kr <wls94kr@gmail.com>
2024-07-12 09:50:12 +0900

    feat: 잔액 조회 기능, 테스트

--- src/test/java/io/hhplus/server_construction/presentation/user/UserControllerTest.java
+++ src/test/java/io/hhplus/server_construction/presentation/user/UserControllerTest.java
@@ -1,14 +1,20 @@
 package io.hhplus.server_construction.presentation.user;
 
 import io.hhplus.server_construction.application.user.facade.UserFacade;
+import io.hhplus.server_construction.domain.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -30,13 +36,16 @@ class UserControllerTest {
     void amount() throws Exception {
         // given
         Long userId = 1L;
+        LocalDateTime now = LocalDateTime.now();
+        User user = User.create(1L, "조진우", BigDecimal.valueOf(1000L), now, now);
 
         // when
+        when(userFacade.findUserById(userId)).thenReturn(user);
         ResultActions response = mockMvc.perform(get(PATH + "/" + userId + "/amount"));
 
         // then
         response.andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.amount").value(100));
+                .andExpect(jsonPath("$.amount").value(1000));
     }
 
     @Test
