4ddb10933eb81529eb37d144227e35e844fe73c1
jo94kr <wls94kr@gmail.com>
2024-08-16 12:13:30 +0900

    feat/test: 결제 - 메시지 발행 / 테스트

--- src/main/java/io/hhplus/server_construction/domain/payment/event/PaymentSuccessEvent.java
+++ src/main/java/io/hhplus/server_construction/domain/payment/event/PaymentSuccessEvent.java
@@ -1,6 +1,23 @@
 package io.hhplus.server_construction.domain.payment.event;
 
-public record PaymentSuccessEvent(
-        String token
-) {
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public final class PaymentSuccessEvent {
+    private final String token;
+    private final String outboxId;
+
+    public PaymentSuccessEvent(
+            String token,
+            String outboxId
+    ) {
+        this.token = token;
+        this.outboxId = outboxId;
+    }
+
+    public static PaymentSuccessEvent create(String token) {
+        return new PaymentSuccessEvent(token, UUID.randomUUID().toString());
+    }
 }
